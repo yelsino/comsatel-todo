27577ae52691a8fbb82f63e953141d4e9d546fcf
yelsino <yelsino321@gmail.com>
2022-06-25 11:08:23 -0500

    separando componente backend y frontend

--- client/src/pages/Taks/OwnComponents/ItemTask.tsx
+++ client/src/pages/Taks/OwnComponents/ItemTask.tsx
@@ -8,7 +8,7 @@ export const ItemTask = ({ task }: Props) => {
     return (
         <div className=" bg-primary-100 px-3 py-3 gap-x-3 grid grid-cols-10/90 select-none  ">
             <span className="text-secondary-100"><IconNotCheck /></span>
-            <p className="text-clip overflow-hidden  align-middle whitespace-pre-wrap break-words text-text-100">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore saepe dolore </p>
+            <p className="text-clip overflow-hidden  align-middle whitespace-pre-wrap break-words text-text-100">{task.text} </p>
         </div>
     )
 }
